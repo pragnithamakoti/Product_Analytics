e9f40683212dafec5d1187790bd7787ed742be21
pragnitha19 <pragnithamakoti@gmail.com>
2026-04-10 22:47:31 +0530

    fixed file path

--- app3.py
+++ app3.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Advanced Product Analytics Dashboard", layout="wi
 st.title("🚀 Advanced Product Analytics Dashboard")
 
 # Load data
-df = pd.read_csv('../data/ecommerce_data.csv')
+df = pd.read_csv('data/ecommerce_data.csv')
 
 # -----------------------
 # Sidebar Filters
